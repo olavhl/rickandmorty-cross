6fe6fdddede8b4c5236d8048105055efd2fcfbdb
olavhl <56083131+olavhl@users.noreply.github.com>
2021-11-22 09:38:04 +0100

    Giving the Tabs some images from FontAwesome5

--- shared/TabNavigation.tsx
+++ shared/TabNavigation.tsx
@@ -1,16 +1,26 @@
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import {NavigationContainer} from "@react-navigation/native";
+import { FontAwesome5 } from "@expo/vector-icons";
 import ListPage from "../pages/ListPage";
 import React from "react";
 
+export type RootStackParamsList = {
+    Home: undefined;
+    Map: undefined;
+}
+
 export function TabNavigation() {
-    const Tab = createBottomTabNavigator();
+    const Tab = createBottomTabNavigator<RootStackParamsList>();
 
 
     return <NavigationContainer>
         <Tab.Navigator>
-            <Tab.Screen name={"Home"} component={ListPage}/>
-            <Tab.Screen name={"Map"} component={ListPage}/>
+            <Tab.Screen name={"Home"} component={ListPage} options={{tabBarIcon: ({color, size}) =>
+                    <FontAwesome5 name={"list"} size={size} color={color} />
+            }}/>
+            <Tab.Screen name={"Map"} component={ListPage} options={{tabBarIcon: ({color, size}) =>
+                    <FontAwesome5 name={"cog"} size={size} color={color} />
+            }}/>
         </Tab.Navigator>
     </NavigationContainer>;
 }
