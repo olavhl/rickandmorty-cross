df7f4586c8ba2ac16097a58b6726b3d3324df243
olavhl <56083131+olavhl@users.noreply.github.com>
2021-11-22 10:46:39 +0100

    Implementing error-handling

--- app/api/RickAndMortyApi.ts
+++ app/api/RickAndMortyApi.ts
@@ -2,21 +2,32 @@ import Axios from 'axios';
 
 Axios.defaults.baseURL = "https://rickandmortyapi.com/api/";
 
+export type Character = {
+    id: number;
+    name: string;
+    species: string;
+    status: string;
+    image: string;
+    origin: {
+        name: string;
+    }
+}
+
 const getAllCharacters = async () => {
     try {
-        const res = await Axios.get("character")
+        const res = await Axios.get<Character>("character")
         return res.data
     } catch (e) {
-        console.log("Error fetching characters from API")
+        throw "Error fetching characters from API " + e
     }
 }
 
 const getCharacter = async (character: number | string) => {
     try {
-        const res = await Axios.get("character")
+        const res = await Axios.get<Character>("character")
         return res.data
     } catch (e) {
-        console.log("Error fetching characters from API")
+        throw "Error fetching characters from API " + e
     }
 }
 
--- app/pages/ListPage.tsx
+++ app/pages/ListPage.tsx
@@ -1,20 +1,37 @@
-import React, {useEffect} from "react";
-import {Text} from "react-native";
-import RickAndMortyApi from "../api/RickAndMortyApi";
-
+import React, {useEffect, useState} from "react";
+import {Button, SafeAreaView, Text} from "react-native";
 const ListPage = () => {
+    const [character, setCharacter] = useState<Character>()
+    const [error, setError] = useState(false)
+
     async function getCharacters() {
-        const characters = await RickAndMortyApi.getAllCharacters();
-        console.log(characters)
+        try {
+            setError(false)
+            const characters = await RickAndMortyApi.getAllCharacters();
+            setCharacter(characters)
+        } catch (e) {
+            setError(true)
+        }
     }
 
     useEffect(() => {
         getCharacters()
     }, [])
 
+    if (error) {
+        return <>
+            <Text>An error did occur</Text>
+            <Button title={"Try again"} onPress={getCharacters} />
+            </>
+    }
+
     return (
-        <Text>Heihei</Text>
-    )
+        <SafeAreaView>
+            <Text>Heihei</Text>
+        </SafeAreaView>
+)
 }
+
+import RickAndMortyApi, {Character} from "../api/RickAndMortyApi";
 
 export default ListPage;
