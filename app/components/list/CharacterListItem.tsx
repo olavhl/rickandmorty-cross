2555bf0c07e5221669f314423ca72ec0bf786155
olavhl <56083131+olavhl@users.noreply.github.com>
2021-11-23 10:45:59 +0100

    Made some simple styling for my list

--- app/components/list/CharacterListItem.tsx
+++ app/components/list/CharacterListItem.tsx
@@ -1,10 +1,23 @@
-import {Text, View} from "react-native";
+import {StyleSheet, Text, View} from "react-native";
 import React from "react";
 import {CharacterProps} from "../../types/Props";
 
 function CharacterListItem({ character }: CharacterProps) {
-    return <View>
-        <Text>{character.name}</Text>
+    const styles = StyleSheet.create({
+        container: {
+            padding: 15,
+            marginLeft: 15,
+            marginRight: 15,
+            borderBottomWidth: 1,
+            borderBottomColor: "gray",
+        },
+        text: {
+            fontSize: 16,
+        }
+    })
+
+    return <View style={styles.container}>
+        <Text style={styles.text}>{character.name}</Text>
     </View>;
 }
 
