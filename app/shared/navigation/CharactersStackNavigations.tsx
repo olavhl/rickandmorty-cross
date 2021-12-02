5be0d3bc0b6cd5f4791043663bc77d3d86475a2c
olavhl <56083131+olavhl@users.noreply.github.com>
2021-12-02 11:45:50 +0100

    Added image as title for homepage

--- app/shared/navigation/CharactersStackNavigations.tsx
+++ app/shared/navigation/CharactersStackNavigations.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Image} from "react-native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import ListPage from "../../pages/ListPage";
 import CharacterDetailsPage from "../../pages/CharacterDetailsPage";
@@ -12,6 +13,15 @@ export type RootStackParamList = {
 const CharactersStackNavigations = () => {
     const Stack = createNativeStackNavigator<RootStackParamList>();
 
+    const BackgroundImage = () => {
+        return (
+            <Image
+                style={{ width: 150, height: 45 }}
+                source={require('../../assets/rickAndMortyTitle.png')}
+            />
+        );
+    }
+
     const options = {
         headerShadowVisible: false,
         headerStyle: {
@@ -19,11 +29,18 @@ const CharactersStackNavigations = () => {
         },
         headerTitleStyle: {
             color: "#FFFFFF",
-        }
+        },
     }
 
     return <Stack.Navigator initialRouteName={"Home"}>
-        <Stack.Screen name={"Home"} component={ListPage} options={options} />
+        <Stack.Screen name={"Home"} component={ListPage} options={{
+            headerShadowVisible: false,
+            headerStyle: {
+                backgroundColor: "#2A2A2A",
+            },
+            headerTitle: () => <BackgroundImage />,
+        }}
+        />
         <Stack.Screen name={"Details"} component={CharacterDetailsPage} options={options} />
     </Stack.Navigator>
 }
