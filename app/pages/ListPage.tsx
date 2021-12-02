e500ffab90958ff16cb0d02db409298daded7f61
olavhl <56083131+olavhl@users.noreply.github.com>
2021-12-02 08:56:20 +0100

    Started styling the searchbar for characters

--- app/pages/ListPage.tsx
+++ app/pages/ListPage.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect} from "react";
-import {FlatList, SafeAreaView} from "react-native";
+import React, {useEffect, useState} from "react";
+import {FlatList, Platform, SafeAreaView, StyleSheet} from "react-native";
 import RickAndMortyApi from "../api/RickAndMortyApi";
 import useApi from "../hooks/useApi";
 import {LoadingView} from "../shared/LoadingView";
@@ -8,10 +8,11 @@ import {ApiProps} from "../types/Props";
 import CharacterListItem from "../components/list/CharacterListItem";
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
 import {RootStackParamList} from "../shared/navigation/CharactersStackNavigations";
-import { SearchBar } from 'react-native-elements';
+import {SearchBar} from 'react-native-elements';
 
 const ListPage = ({navigation}: NativeStackScreenProps<RootStackParamList>) => {
     const {data, error, loading, request: getCharacters} = useApi<ApiProps>(RickAndMortyApi.getAllCharacters)
+    const [search, setSearch] = useState("")
     const globalStyle = require("../assets/style");
 
     useEffect(() => {
@@ -26,15 +27,45 @@ const ListPage = ({navigation}: NativeStackScreenProps<RootStackParamList>) => {
         return <ErrorView onPress={() => getCharacters}/>
     }
 
+    const updateSearch = (searchWord: string) => {
+        setSearch(searchWord)
+        console.log(search)
+    }
+
     return (
         <SafeAreaView style={globalStyle.mainBackground}>
+            <SearchBar
+                placeholder={"Search for character"}
+                platform={Platform.OS === 'ios' ? 'ios' : 'android'}
+                value={search}
+                inputContainerStyle={[styles.searchBarContainer, styles.searchField]}
+                containerStyle={[styles.searchBarContainer]}
+                searchIcon={{color: "white"}}
+                style={styles.searchBarContainer}
+                onChangeText={(text: string) => updateSearch(text)}
+
+            />
             {data && <FlatList data={data.results}
                                keyExtractor={(item) => item.id.toString()}
                                renderItem={({item}) => <CharacterListItem
-                               onPress={() => navigation.navigate("Details", {character: item})} character={item}/>}
+                                   onPress={() => navigation.navigate("Details", {character: item})} character={item}/>}
             />}
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    searchBarContainer: {
+        backgroundColor: "#2A2A2A",
+        color: "white"
+
+    },
+    searchField: {
+        borderColor: "black",
+        borderWidth: 1,
+        borderRadius: 10,
+        marginBottom: 10,
+    }
+})
 
 export default ListPage;
